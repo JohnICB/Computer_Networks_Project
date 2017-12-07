a9e3fe3e64e67954d3bb7866ca1d6f8ddbb49d65
JohnFII <ionuticb@gmail.com>
2017-12-07 11:00:47 +0200

    Resolving merge conflict part 2

--- client.c
+++ client.c
@@ -1,3 +1,6 @@
+
+//client.c
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -18,7 +21,6 @@ int str_echo(int server_fd); //takes care of the communication
 int main()
 {
 	int server_fd;
-<<<<<<< HEAD
 	struct sockaddr_in server;
 /*creating socket*/
 	if ( (server_fd = socket(AF_INET,SOCK_STREAM,0)) == -1)
@@ -28,20 +30,12 @@ int main()
 		}
 
 
-=======
-	struct sockaddr_in cli;
-	puts("I am Client...");
-/*creating socket*/
-	server_fd=socket(AF_INET,SOCK_STREAM,0);
-	puts("Socket Created Successfully...");
->>>>>>> master
 /*socket address structure*/
 	server.sin_family=AF_INET;
 	server.sin_addr.s_addr=inet_addr("127.0.0.1");
 	server.sin_port=htons(5000);
 
 /*connecting to server*/
-<<<<<<< HEAD
 	if (connect (server_fd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
     {
       perror ("Error connecting()\n");
@@ -101,63 +95,4 @@ int str_echo(int server_fd)
 	}while(bytes > 0);
  
 
-=======
-	connect(server_fd,(struct sockaddr*)&cli,sizeof(cli));
-	puts("Connected with Server...");
-	//printf("%d returned \n",str_echo(server_fd));
-	str_echo(server_fd);
-	close(server_fd);
-	return 0;
-}
-
-int str_echo(int server_fd)
-{
-	char buf[BUFF_SIZE];//,buf1[BUFF_SIZE];
-	do{
-	bzero(buf, BUFF_SIZE);
-	//puts("Enter the Message...");
-	//fgets(buf,BUFF_SIZE,stdin);
-	//send(server_fd,buf,BUFF_SIZE,0);
-	//recv(server_fd,buf1,BUFF_SIZE,0);
-	//puts("Message from Server...");
-	//fputs(buf1,stdout);
-	
-	printf("Enter the Message...\n");
-	
-	if (read(0, buf, BUFF_SIZE) < 0)
-	{
-		perror ("Error reading from stdin\n");
-		return errno;
-	}
-
-	//printf("%s was lalala \n", buf);
-	//fputs(buf, BUFF_SIZE);
-	//sending data to server
-	if (strlen(buf) > 2)
-	{
-		/* code */
-	}
-	buf[strlen(buf) - 1] = '\0';
-	//write(0, buf, BUFF_SIZE);
-
-	if (write (server_fd, buf, BUFF_SIZE) < 0)
-    {
-      perror ("Error while writing to the Server.\n");
-      return errno;
-  	}
-
-  	int sz;
-	//receiving data from server
-	sz = read(server_fd, buf, BUFF_SIZE);
-	if (sz < 0)
-	{
-		perror ("Error reading from stdin\n");
-		return errno;
-	}
-
-	printf("Message from Server...  %s\n",buf);
-	//snprintf(stdout, sz,"Message fromm Server... %s\n", buf);
-
-	}while(strlen(buf)>1);
->>>>>>> master
 }
